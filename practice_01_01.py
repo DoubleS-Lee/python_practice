4697cc0912444ec680e862e5a35746631d097e61
DoubleS-Lee <lss8825@gmail.com>
2020-08-07 00:26:02 +0900

    200807_업로드

---
+++ practice_01_01.py
@@ -0,0 +1,139 @@
+# 초보자를 위한 파이썬 300제
+# https://wikidocs.net/7014
+
+# 1
+print('Hello World')
+
+# 2
+print("Mary's cosmetics")
+
+# 3
+print('신씨가 소리질렀다. "도둑이야".')
+
+# 4
+print("c:\Windows")
+
+# 5
+print("안녕하세요.\n만나서\t\t반갑습니다.")
+
+# 6
+print("오늘은", "일요일")
+
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! 7
+
+# 8
+print('naver','kakao','sk','samsung',sep='/')
+
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! 9
+
+
+# 10
+print(5/3)
+
+# 11
+삼성전자 = 50000
+보유주 = 10
+총평가금액 = 삼성전자 * 보유주
+print(총평가금액)
+
+# 12
+시가총액 = 298000000000000
+현재가 = 50000
+PER = 15.79
+
+# 13
+s="hello"
+t='python'
+
+print(f'{s}! {t}')
+
+# 14
+2+2*3
+
+# 15
+a='128'
+type(a)
+
+# 16
+num_str = "720"
+int(num_str)
+
+# 17
+num=100
+type(str(num))
+
+# 18
+a = '15.79'
+type(float(a))
+
+# 19
+year = "2020"
+year = int(year)
+print(f"{year - 3} {year - 2} {year - 1}")
+
+# 20
+air_price = 48584
+month = 36
+total_price = air_price * month
+print(total_price)
+
+# 21
+letters = 'python'
+print(letters[0], letters[2])
+
+# 22
+license_plate = "24가 2210"
+print(license_plate[-4:])
+
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! 23
+string = "홀짝홀짝홀짝"
+
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! 24
+string = "PYTHON"
+
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! 25
+phone_number = "010-1111-2222"
+
+# 26
+phone_number = phone_number.replace(" ","")
+print(phone_number)
+
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! 27
+url = "http://sharebook.kr"
+
+# 28
+
+# 29
+string = 'abcdfe2a354a32a'
+string = string.replace('a', 'A')
+print(string)
+
+# 30
+# 31
+# 32
+
+# 33
+print('-'*80)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
